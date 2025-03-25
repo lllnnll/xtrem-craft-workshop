3984e7fe320ceca057c463f255bacfe76b843994
Mathis Durand <mathis.durand.1@etu.u-bordeaux.fr>
2025-03-25 17:56:39 +0100

    04

--- java/src/test/java/money_problem/domain/BankTest.java
+++ java/src/test/java/money_problem/domain/BankTest.java
@@ -10,19 +10,30 @@ class BankTest {
 
     @Test
     void convert_eur_to_usd_returns_double() throws MissingExchangeRateException {
-        assertThat(Bank.withExchangeRate(EUR, USD, 1.2).convert(10, EUR, USD))
-                .isEqualTo(12);
+        // Arrange
+        Bank bank = Bank.withExchangeRate(EUR, USD, 1.2);
+
+        // Act
+        double amount = bank.convert(10, EUR, USD);
+
+        // Assert
+        assertThat(amount).isEqualTo(12);
     }
 
     @Test
     void convert_eur_to_eur_returns_same_value() throws MissingExchangeRateException {
-        assertThat(Bank.withExchangeRate(EUR, USD, 1.2).convert(10, EUR, EUR))
+        Bank bank = Bank.withExchangeRate(EUR, USD, 1.2);
+        double convert = bank.convert(10, EUR, EUR);
+        assertThat(convert)
                 .isEqualTo(10);
     }
 
     @Test
     void convert_throws_exception_on_missing_exchange_rate() {
-        assertThatThrownBy(() -> Bank.withExchangeRate(EUR, USD, 1.2).convert(10, EUR, KRW))
+        assertThatThrownBy(() -> {
+            Bank bank = Bank.withExchangeRate(EUR, USD, 1.2);
+            double convert = bank.convert(10, EUR, KRW);
+        })
                 .isInstanceOf(MissingExchangeRateException.class)
                 .hasMessage("EUR->KRW");
     }
