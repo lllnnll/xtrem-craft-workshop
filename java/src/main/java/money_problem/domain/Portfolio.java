f7d4f98d9de79c9ae5949b02d26306e35561e4d1
Nikita Sitchevoy <nikita.sitchevoy@etu.u-bordeaux.fr>
2025-06-17 14:56:04 +0200

    9 -> remove of old methods + refacto of add money

--- java/src/main/java/money_problem/domain/Portfolio.java
+++ java/src/main/java/money_problem/domain/Portfolio.java
@@ -5,31 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Portfolio {
-    private Map<Currency, Double> amounts = new HashMap<>();
     private ArrayList<Money> moneys = new ArrayList<>();
 
     public Portfolio() {}
-
-    public void add(double amount, Currency currency) {
-        // Implementation for adding an amount in a specific currency
-        if (!amounts.containsKey(currency)){
-            amounts.put(currency, amount);
-        }else{
-            amounts.put(currency, amounts.get(currency) + amount);
-        }
-        moneys.add(new Money(amount, currency));
-    }
-
-    public double old_evaluate(Bank bank, Currency currency) throws MissingExchangeRateException {
-        double total = 0;
-        for(var amount : amounts.entrySet()) {
-            Currency sourceCurrency = amount.getKey();
-            double sourceAmount = amounts.get(sourceCurrency);
-            Money sourceMoney = new Money(sourceAmount, sourceCurrency);
-            total += bank.convert(sourceMoney, currency).amount();
-        }
-        return total;
-    }
 
     public double evaluate(Bank bank, Currency currency) throws MissingExchangeRateException {
         Money total = new Money(0, currency);
@@ -40,6 +18,6 @@ public class Portfolio {
     }
 
     public void add(Money money) {
-        add(money.amount(), money.currency());
+        moneys.add(money);
     }
 }
